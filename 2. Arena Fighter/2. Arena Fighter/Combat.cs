b817b4ccbb60ac2e996aa8315c60153cfdf2e339
YoussefSweden <79831898+YoussefSweden@users.noreply.github.com>
2021-04-26 08:03:58 +0200

    Add files via upload

---
+++ 2. Arena Fighter/2. Arena Fighter/Combat.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+//using System.Text;
+
+namespace ArenaFighter
+{
+    class  Combat
+    {
+        static Random slump = new Random();
+        public int strength = slumpGenerator();
+        public static int battlePlayerCount = 0;
+        public static int battleMotståndareCount = 0;
+        public int BattlePlayerPoint()
+        {
+            return battlePlayerCount += 1;
+        }
+        public static int slumpGenerator()
+        {
+            return slump.Next(1, 6);
+        }
+        public int BattleMotståndareCount()
+        {
+            return battleMotståndareCount += 1;
+        }
+
+        public string Name { get; set; }
+        public int Power { get; set; }
+        public int Health { get; set; }
+    }
+    class Motståndare : Combat
+    {
+        public Motståndare()    
+        {
+            int i=slumpGenerator();
+            string[] moståndareArray = { "Berra","Tore", "Agda", "Berit","Gunnar", "Astrid" };
+
+            Name = moståndareArray[i];
+            Health = 5;
+        }
+        public int Kraft()
+        {
+            return Power;
+        }
+    }
+    class Player : Combat
+    {
+        public  Player()
+        {
+            Name = "";
+            Power = slumpGenerator();
+            Health = 5;
+        }
+    }
+}
---
+++ 2. Arena Fighter/2. Arena Fighter/Spelet.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArenaFighter
+{
+    class Spelet
+    {
+        public static void Spela()
+        {
+            Console.WriteLine();
+
+            var Player = new Player();
+            int playerHealth = Player.Health;
+            Console.Write("Vem är du som spelar? ");
+            Player.Name = Console.ReadLine();
+
+            int playerBattlePoäng = Player.BattlePlayerPoint();
+            bool fight = true;
+            string inMatatVal = "";
+
+            while (fight == true)
+            {
+                Console.WriteLine("1. New Battle");
+                Console.WriteLine("0. Exit, No  fighting ");
+                Console.WriteLine();
+                inMatatVal = Console.ReadLine();
+                switch (inMatatVal)
+                {
+                    case "1":
+                        int playerPoäng = 0; ;
+                        int motståndarePoäng = 0;
+                        int playerNewPoäng = 0;
+
+                        var motståndare = new Motståndare();
+                        int motståndareHealth = motståndare.Health;
+                        int motståndareBattlePoäng = motståndare.BattleMotståndareCount();
+                        string slåssMer = "";
+
+                        while (slåssMer != "n" && playerHealth > 0 && motståndareHealth > 0)
+                        {
+                            Console.WriteLine("Wanna fight? (j/n)");
+
+                            slåssMer = Console.ReadLine().ToLower();
+                            Console.Clear();
+                            switch (slåssMer)
+                            {
+                                case "j":
+                                    string motstånd = motståndare.Name;
+                                    int playerSlag = Combat.slumpGenerator();
+                                    int motståndareSlag = Combat.slumpGenerator();
+
+                                    if (playerSlag > motståndareSlag)
+                                    {
+                                        motståndareHealth--;
+                                        playerPoäng++;
+                                        playerPoäng += playerNewPoäng;
+                                    }
+                                    else
+                                    {
+                                        playerHealth--;
+                                        motståndarePoäng++;
+                                    }
+                                    Console.WriteLine($"{Player.Name} Health: {playerHealth}");
+                                    Console.WriteLine($"{motstånd} Health : {motståndareHealth}\n");
+
+                                    Console.WriteLine("\n" + Player.Name + " score: " + playerPoäng);
+                                    Console.WriteLine("\n" + motstånd + " score: " + motståndarePoäng);
+
+                                    if (motståndareHealth == 0 || playerHealth == 0 && motståndareHealth == 0)
+                                    {
+                                        Console.WriteLine("\nThe Battle is over");
+                                        Console.WriteLine(Player.Name + " won and have survived: " + playerBattlePoäng + " battle");
+                                    }
+
+                                    else if ((motståndareHealth == 0 || playerHealth == 0 && playerHealth == 0))
+                                    {
+                                        Console.WriteLine("\nThe Battle is over");
+                                        Console.WriteLine(motstånd + " won after  : " + playerBattlePoäng + " battle");
+                                    }
+                                    break;
+
+                                case "n":
+                                    motståndareHealth = 0;
+                                    Console.WriteLine("\nChicken go home \n");
+                                    break;
+                                default:
+                                    Console.WriteLine("\nFel val, försök igen");
+                                    break;
+                            }
+                        }
+                        break;
+                    case "0":
+                        fight = false;
+                        break;
+                        
+                    default:
+                        Console.WriteLine("Felaktigt val!");
+                        break;
+                }
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
+
+
+    
